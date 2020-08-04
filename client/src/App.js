3538ed406bb2dace2c672b08faa4aadae3efab28
Ido Calman <calman.ido@gmail.com>
2020-08-04 19:28:34 +0300

    Client config is no longer necessary

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import CircularProgress from '@material-ui/core/CircularProgress';
 import PatientInformation from "./views/PatientInformation";
 import Results from "./views/Results";
-import config from "./config"
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/shards-dashboards.1.1.0.min.css";
@@ -30,8 +29,7 @@ export default function() {
   async function getAnalysis({age, gender, startState, states}) {
     let start_state_type = stateType(startState.medicalState)
     setAppState({analysis: {}, isLoading: true})
-    let url = config['API_HOST'] + "patient"
-    const response = await fetch(url, {
+    const response = await fetch("/patient", {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
